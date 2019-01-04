ac2f27d37fc18d7c02d93993c30af8237be2ac4d
dcasanova82 <dante.casanova@gmail.com>
2019-01-04 18:47:35 -0500

    first commmit

--- sec-groups.py
+++ sec-groups.py
@@ -1,13 +1,32 @@
 import boto3
 
-ec2=boto3.resource('ec2')
-security_groups=ec2.security_groups.all()
+ec2 = boto3.resource('ec2')
+security_groups = ec2.security_groups.all()
 
-for security_group in security_groups:
-    for rule in security_group.ip_permissions:
-        port = str(rule['FromPort'])
-        protocol = rule['IpProtocol']
-        for ip_address in rule['IpRanges']:
-            if ip_address['CidrIp'] == '195.26.158.190/32':
 
-                print(security_group.group_name, port, protocol, ip_address['CidrIp'])
+ip_add = '195.26.158.190/32'
+
+
+def search_groups_by_ip(ip):
+    for security_group in security_groups:
+        for rule in security_group.ip_permissions:
+            for ip_address in rule['IpRanges']:
+                if ip_address['CidrIp'] == ip:
+                    if rule['IpProtocol'] == '-1':
+                        protocol = 'All'
+                        print(security_group.group_id, security_group.group_name, protocol, ip_address['CidrIp'])
+                        # security_group.revoke_ingress(CidrIp=ip, IpProtocol=rule['IpProtocol'])
+                    else:
+                        fromPort = rule['FromPort']
+                        toPort = rule['ToPort']
+                        protocol = rule['IpProtocol']
+                        print(security_group.group_id, security_group.group_name, protocol, fromPort, toPort, ip_address['CidrIp'])
+                        # security_group.revoke_ingress(CidrIp=ip, IpProtocol=rule['IpProtocol'], FromPort=rule['FromPort'], ToPort=rule['ToPort'])
+                else:
+                    break
+    print('IP not found in Security Groups')
+
+search_groups_by_ip(ip_add)
+
+# sec_group = ec2.SecurityGroup('sg-233bde4a').revoke_ingress(CidrIp='195.26.158.190/32', IpProtocol='tcp', FromPort=0, ToPort=65535)
+# print(sec_group.group_name)
