9868565c6b8f41c0a8c6c3f082797e04f2187704
dcasanova82 <dante.casanova@gmail.com>
2019-01-21 11:55:03 -0500

    defined folder path by date and handler

--- export-logs.py
+++ export-logs.py
@@ -1,31 +1,26 @@
 import boto3
-from datetime import datetime, timezone
+from datetime import datetime, timedelta
 
 
 client = boto3.client('logs')
 
-# response = client.create_export_task(
-#     taskName='logs-to-s3-task',
-#     logGroupName='SOAPlog',
-#     fromTime=1547510400000,
-#     to=1547769600000,
-#     destination='lamark-log-stream',
-#     destinationPrefix='SOAP-logs'
-# )
-fromTime = datetime(2019,1,17,0,0,0,0).timestamp()
-to = datetime(2019,1,17,23,59,59).timestamp()
-print(fromTime, int(to))
 
-dt_object = datetime.fromtimestamp(1547769599)
-print(dt_object)
-
-response = client.describe_export_tasks(
-    taskId='68c766ae-a1a9-4488-947c-f0fdf8f8e88e'
-)
-
-print("Response of logs-to-S3 lambda function: ", response)
+def get_date_path():
+    year = datetime.now().year
+    month = datetime.now().month
+    day = datetime.now().day
+    date_path = str(year)+'/'+str(month)+'/'+str(day)
+    return date_path
 
 
-# 1547683200000
-# 1547769599000
-# 1547769599.0
+def lambda_handler(event, context):
+    response = client.create_export_task(
+        taskName='logs-to-s3-task',
+        logGroupName=event["logGroupName"],
+        fromTime=int(round((datetime.now() - timedelta(days=1)).timestamp() * 1000)),
+        to=int(round(datetime.now().timestamp() * 1000)),
+        destination='lamark-log-stream',
+        destinationPrefix=event["logGroupName"]+'/'+get_date_path()
+    )
+
+    print("Response of logs-to-s3 lambda function: " + response)
